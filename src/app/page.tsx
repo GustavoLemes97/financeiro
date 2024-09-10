bef317b3614e91a78509835c471a69ab874dd4d6
Gustavo Lemes <gustavolemes997@gmail.com>
2024-09-10 19:36:06 -0300

    cria texto de profit do card profit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
 "use client";
-import * as React from "react";
-import { Button } from "@/components/ui/button";
-import { DatePicker } from "@/components/ui/datePicker";
-import { InputWithLabel } from "@/components/ui/inputWithLabel";
-import { Select } from "@/components/ui/select";
-import { TablePagination } from "@/components/ui/tablePagination";
+
+import { ProfitText } from "@/components/ui/profitText";
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = React.useState(1);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-  return (
-    <main className="p-10 flex flex-col gap-4">
-      <TablePagination
-        onPageChange={handlePageChange}
-        quantityData={102}
-        limitDataForPage={10}
-      />
-    </main>
-  );
+  return <ProfitText oldMonthTotal={10} currentMonthTotal={15} />;
 }
