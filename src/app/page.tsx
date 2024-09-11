5fc0e9ef2dc0162ed03bb87286e0a33bcb6fbf46
Gustavo Lemes <gustavolemes997@gmail.com>
2024-09-11 16:37:50 -0300

    muda a forma de formatar a data do datepicker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 "use client";
-
-import { AlertCard } from "@/components/ui/alertCard";
+import * as React from "react";
+import { DatePicker } from "@/components/ui/datePicker";
 
 export default function Home() {
-  const warningsData = [
-    { name: "Aluguel", date: new Date(2024, 8, 10) },
-    { name: "Cartão", date: new Date(2024, 8, 15) },
-    { name: "Tratamento de esgoto", date: new Date(2024, 8, 20) },
-    { name: "Churrasco", date: new Date(2024, 8, 25) },
-    { name: "Manutenção programada", date: new Date(2024, 8, 30) },
-  ];
+  const [date, setDate] = React.useState<Date>();
+  const handleonSelect = (tdate: Date | undefined) => {
+    setDate(tdate);
+    console.log("teste", tdate);
+  };
   return (
-    <>
-      <AlertCard title={"Pagamentos a vencer"} warnings={warningsData} />
-    </>
+    <DatePicker
+      mode="single"
+      date={date}
+      onSelect={handleonSelect}
+      selected={date}
+    />
   );
 }
--- src/components/ui/datePicker.tsx
+++ src/components/ui/datePicker.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
 import { Calendar as CalendarIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar, CalendarProps } from "@/components/ui/calendar";
@@ -12,6 +9,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { formatDate } from "@/utils/dateUtils";
 
 type DatePickerProps = CalendarProps & {
   date: Date | undefined;
@@ -24,13 +22,13 @@ const DatePicker = ({ date, ...props }: DatePickerProps) => {
         <Button
           variant={"filter"}
           className={cn(
-            "w-auto max-w-[300px] justify-start text-left font-normal px-4 py-2",
+            "w-[280px] justify-start text-left font-normal px-4 py-2",
             !date && "text-muted-foreground"
           )}
         >
           <CalendarIcon className="h-5 w-5 text-soft-grey-2 self-center" />
           {date ? (
-            format(date, "dd 'de' MMMM 'de' yyyy", { locale: ptBR })
+            formatDate(date)
           ) : (
             <span className="text-base lg:text-xl font-normal">
               Selecionar Data
