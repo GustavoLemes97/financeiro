ae17ac9c7ea7917426cee71dde6b1ca50f38cf64
Gustavo Lemes <gustavolemes997@gmail.com>
2024-09-30 12:26:56 -0300

    cria um serviço fake para api e aplicar o zustand na aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { EntriesTable } from "@/components/ui/entriesTable";
+import useEntriesStore from "@/zustand/useEntriesStore";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { entries, fetchEntriesFromAPI } = useEntriesStore();
+
+  useEffect(() => {
+    fetchEntriesFromAPI();
+  }, [fetchEntriesFromAPI]);
+
   return (
     <main className="bg-soft-grey p-4">
-      <EntriesTable />
+      <EntriesTable data={entries} />
     </main>
   );
 }
